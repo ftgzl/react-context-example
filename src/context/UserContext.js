63a1bb4a34d49e9a4895943e875bfebca3ad43b4
fuatguzel <fuatguzel754@gmail.com>
2021-09-08 23:04:50 +0300

    Add UserContext

---
+++ src/context/UserContext.js
@@ -0,0 +1,22 @@
+import {createContext, useState} from 'react'
+
+const UserContext = createContext()
+
+export const UserProvider = ({children}) => {
+  const [user, setUser] = useState({
+    id: 1,
+    userName : 'Fuat',
+  })
+
+  const values = {
+    user,
+    setUser
+  }
+
+  return <UserContext.Provider value={values}>
+    {children}
+  </UserContext.Provider>
+}
+
+
+export default UserContext;
